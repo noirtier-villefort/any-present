567d2f25124fd6d3629d9a635c7a57389ce23951
noirtier-villefort <polusash@gmail.com>
2025-02-15 03:49:26 +0100

    init

---
+++ bash.bat
@@ -0,0 +1,15 @@
+@echo off
+color 0C # color here
+chcp 65001 # for cyryllic text
+echo    *****     *****
+echo   *******   *******
+echo  ********* *********
+echo *********************
+echo  *******************
+echo    ***************
+echo      ***********
+echo        *****
+echo         ***
+echo          *
+echo your text here
+pause
